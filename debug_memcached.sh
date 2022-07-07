9556575e146ee6bb6858e07f8e42ab020020a421
Haosen Wen <hwen5@node1x10a.cs.rochester.edu>
2022-07-07 10:40:43 -0400

    update debug script

--- debug_memcached.sh
+++ debug_memcached.sh
@@ -1,4 +1,7 @@
 #/bin/bash
 
-./bin/ycsb load memcached -s -P workloads/workloada -p "memcached.hosts=127.0.0.1" > output.txt
-./bin/ycsb run memcached -s -P workloads/workloada -p "memcached.hosts=127.0.0.1" > output.txt 
+RECORD_COUNT=10000
+OP_COUNT=10000
+
+./bin/ycsb load memcached -s -P workloads/workloada -p "memcached.hosts=127.0.0.1" -p recordcount=$RECORD_COUNT > output.txt
+./bin/ycsb run memcached -s -P workloads/workloada -p "memcached.hosts=127.0.0.1" -p operationcount=$OP_COUNT > output.txt 
